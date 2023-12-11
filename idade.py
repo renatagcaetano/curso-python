9f3779aa80abf7aab1ababc77fa5a3522fb0cc41
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-11 10:33:24 -0300

    Calcular a idade com o Input

---
+++ idade.py
@@ -0,0 +1,6 @@
+ano_nascimento = input('Informe o ano de seu nascimento: ')
+ano_atual = input('Informe o ano atual: ')
+
+idade = int(ano_atual) - int(ano_nascimento)
+
+print('Idade: ' + str(idade))
