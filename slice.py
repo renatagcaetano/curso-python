70a75b0f620fe2cec81f32b97b01a0f64f8db9e8
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-11 16:53:27 -0300

    Slice

---
+++ slice.py
@@ -0,0 +1,18 @@
+fruta = 'Abacate'
+#index   0123456
+#index   -7-6-5-4-3-2-1
+
+print(fruta)
+print(fruta[3])  # c
+print(fruta[0])  # A
+print(fruta[2:5])  # aca
+print(fruta[2:6])  # acat
+print(fruta[-1])  # e
+
+valor = 99.75
+#index  01234
+valor = str(valor)
+
+print(valor[3:5])  # 75
+print(valor[:2])  # 99
+print(valor[2:])  # .75
