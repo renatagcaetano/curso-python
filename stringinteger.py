f2f694ccacdcf0b61213ab1b9f4cb5bc2c581de5
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-11 09:44:48 -0300

    Praticando com Strings e Integers

---
+++ stringinteger.py
@@ -0,0 +1,7 @@
+# Criar frase, adicionar variáveis e imprimir
+
+nome = 'André'
+idade = 32  # idade = str(idade)
+cidade = 'São Paulo'
+
+print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' + cidade + '.')
