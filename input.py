92939e74bffb5e4fa78fd628f85555b2c8f833dc
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-11 10:04:48 -0300

    Input

---
+++ input.py
@@ -0,0 +1,8 @@
+# Criar frase, adicionar variáveis e imprimir
+
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+# idade = str(idade)
+cidade = input('Onde você mora? ')
+
+print(nome + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' + cidade + '.')
