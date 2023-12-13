64c00b7e85943851c39885aa37cef5b181fbf383
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-13 11:16:07 -0300

    Operadores de comparação

---
+++ comparacao.py
@@ -0,0 +1,38 @@
+operacao1 = 10 == 10
+print(operacao1)
+
+operacao2 = 10 == 11
+print(operacao2)
+
+operacao3 = 'banana' == 'banana'
+print(operacao3)
+
+operacao4 = 'banana' == 'Banana'
+print(operacao4)
+
+operacao5 = 'banana' != 'Banana'
+print(operacao5)
+
+operacao6 = 'banana' != 'banana'
+print(operacao6)
+
+operacao7 = 10 > 13
+print(operacao7)
+
+operacao8 = 10 > 9
+print(operacao8)
+
+operacao9 = 10 < 9
+print(operacao9)
+
+operacao10 = 10 < 93
+print(operacao10)
+
+operacao11 = 10 <= 93
+print(operacao11)
+
+operacao12 = 10 <= 9
+print(operacao12)
+
+operacao13 = 10 <= 10
+print(operacao13)
