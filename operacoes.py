54f86c62f59914d24c128df7f0a4dfb2d47a77df
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-13 10:45:32 -0300

    Operações matemáticas

---
+++ operacoes.py
@@ -0,0 +1,19 @@
+calculo1 = 2 + 2
+
+print(calculo1)
+
+calculo2 = 2 + 2 * 3
+
+print(calculo2)
+
+calculo3 = (2 + 2) * 3
+
+print(calculo3)
+
+calculo4 = 2**3
+
+print(calculo4)
+
+calculo5 = 2**3 + 4
+
+print(calculo5)
