8108f2737151ccba6c0ebca884e06905318784a1
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-13 10:24:47 -0300

    Métodos para Strings

---
+++ metodosstring.py
@@ -0,0 +1,20 @@
+mensagem = 'Eu adoro comida Caseira'
+
+print(mensagem)
+print(mensagem.lower())
+print(mensagem.upper())
+print(mensagem.find('c'))
+print(mensagem.find('C'))
+print(mensagem.find('adoro'))
+print(mensagem.find('aDoro'))
+print(mensagem.replace('a', 'e'))
+print(mensagem.replace('Caseira', 'feita em casa'))
+
+mensagem2 = 'eu adoro comida Caseira'
+
+print(mensagem2.capitalize())
+
+mensagem3 = '     Eu adoro comida caseira'
+
+print(mensagem3)
+print(mensagem3.strip())
