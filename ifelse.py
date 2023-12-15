6583efc62249f6c5163174844d850066667e8255
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-15 15:34:31 -0300

    If, elif, else

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+velocidade = 100
+
+if velocidade > 110:
+    print('Acima da velocidade permitida.')
+    print('Reduza a sua velocidade.')
+elif velocidade < 60:
+    print('Dirija acima de 80km/h')
+else:
+    print('Velocidade permitida.')
