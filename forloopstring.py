cbccabaee374dc008b1c1e1b604c64ed2cef6c72
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-15 16:48:50 -0300

    For Loop com String

---
+++ forloopstring.py
@@ -0,0 +1,10 @@
+'''
+    for letra in 'Google':
+        print(letra)
+'''
+
+palavra = 'Fantástico'
+
+for letra in palavra:
+    # print(letra + ' está dentro da palavra Google')
+    print(f'{letra} está dentro da palavra {palavra}')
