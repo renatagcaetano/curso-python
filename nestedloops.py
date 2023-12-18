2733ce0cc2c34f814d20ef30c71591e41c66d2d1
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-18 14:51:09 -0300

    Nested loops

---
+++ nestedloops.py
@@ -0,0 +1,12 @@
+'''
+for numero1 in range(5):
+    print(numero1)
+    for numero2 in range(5):
+        # print(numero2)
+        print(numero1, numero2)
+'''
+
+for numero1 in range(1, 6):
+    print('Produto ' + str(numero1))
+    for numeor2 in range(11):
+        print(numero1, numeor2)
