2edafb2dda3fc40c7d42a46dff5cc0ef89cb611d
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-19 15:43:27 -0300

    Parâmetros e argumentos

---
+++ parametroargumento.py
@@ -0,0 +1,8 @@
+def boas_vindas(nome, quantidade):
+    print(f'Olá, {nome}!')
+    print(f'Há {str(quantidade)} laptops em estoque')
+
+
+boas_vindas('Marcos', 5)
+boas_vindas('Ronaldo', 4)
+boas_vindas('Linda', 2)
