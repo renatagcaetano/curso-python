a4341186059cf44180ff69dd00f68ed17e681963
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-12-19 15:32:09 -0300

    Função soma

---
+++ funcaosoma.py
@@ -0,0 +1,8 @@
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+
+primeiro = int(input("Digite o primeiro número: "))
+segundo = int(input("Digite o segundo número: "))
+
+print(f'A soma dos números informados e: {soma(primeiro, segundo)}')
