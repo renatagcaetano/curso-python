38d6101e714c55ef992fe16d9631dd05d0b7c838
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-08 08:43:02 -0300

    Nomeando parâmetros com xargs

---
+++ parametroxargs.py
@@ -0,0 +1,7 @@
+def agencia(**carro):
+    return carro
+
+
+print(agencia(marca='Gol', cor='Branco', motor=1.0, placa='1234'))
+print(agencia(marca='Gol', cor='Azul', motor=1.0))
+print(agencia(marca='Gol', cor='Preto'))
