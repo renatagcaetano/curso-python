f323eab9c0b37d01f360ceedbcf2bfd40aa453c4
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-08 15:37:04 -0300

    Concatenação de listas

---
+++ concatenarlistas.py
@@ -0,0 +1,24 @@
+numeros = [2, 3, 4, 5]
+letras = ['a', 'b', 'c', 'd']
+
+print(numeros[2])
+
+final = numeros * 2
+print(final)
+
+final = numeros * 4
+print(final)
+
+final = numeros + letras
+print(final)
+
+numeros.extend(letras)
+print(numeros)
+
+itens = [['item1', 'item2'], ['item3', 'item4']]
+
+print(itens)
+print(itens[0])
+print(itens[1])
+print(itens[0][1])
+print(itens[1][1])
