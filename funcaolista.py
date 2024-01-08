dac29bfb0d600208640631bab34525d8ec96002c
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-08 15:23:44 -0300

    Funções para listas

---
+++ funcaolista.py
@@ -0,0 +1,17 @@
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiânia']
+print(cidades)
+
+cidades.append('Florianópolis')
+print(cidades)
+
+cidades.remove('Salvador')
+print(cidades)
+
+cidades.insert(1, 'Fortaleza')
+print(cidades)
+
+cidades.pop(0)
+print(cidades)
+
+cidades.sort()
+print(cidades)
