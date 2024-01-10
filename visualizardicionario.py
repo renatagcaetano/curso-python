3cb934f4d4d1abdce2023596c716be338737f40e
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-10 10:15:34 -0300

    Visualizar itens, keys e values

---
+++ visualizardicionario.py
@@ -0,0 +1,15 @@
+aluno = {
+    'nome': 'Ana',
+    'idade': 16,
+    'nota_final': 10.0,
+    'aprovação': True,
+    'matérias': ['Física', 'Matemática', 'Inglês']
+}
+
+print(aluno)
+print(aluno.get('matérias'))
+print(len(aluno))
+
+print(aluno.items())
+print(aluno.keys())
+print(aluno.values())
