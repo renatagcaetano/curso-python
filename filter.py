e44463c916d3060d5259ce2b2209ec7961f4b506
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-11 08:52:05 -0300

    Filter

---
+++ filter.py
@@ -0,0 +1,10 @@
+valores = [10, 12, 34, 44, 57]
+
+
+def remover20(x):
+    return x > 20
+
+
+print(list(map(remover20, valores)))
+print(list(filter(remover20, valores)))
+print(list(filter(lambda x: x > 20, valores)))
