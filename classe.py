f9a83392f0f9a1ac64b88cce960ca8687e4f755b
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-12 11:00:43 -0300

    Classe

---
+++ classe.py
@@ -0,0 +1,11 @@
+class Funcionarios:
+    nome = 'Helena'
+    sobrenome = 'Cabral'
+    data_de_nascimento = '12/01/2009'
+
+
+funcionario1 = Funcionarios()
+
+print(funcionario1.nome)
+print(funcionario1.sobrenome)
+print(funcionario1.data_de_nascimento)
