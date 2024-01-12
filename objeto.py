d1254958e26f111d9eb0ac9a8149b8e112d88cb6
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-12 11:13:45 -0300

    Objeto

---
+++ objeto.py
@@ -0,0 +1,19 @@
+class Funcionarios:
+    nome = ''
+    sobrenome = ''
+    data_de_nascimento = ''
+
+
+funcionario1 = Funcionarios()
+funcionario2 = Funcionarios()
+
+funcionario1.nome = 'Helena'
+funcionario1.sobrenome = 'Cabral'
+funcionario1.data_de_nascimento = '12/01/2009'
+
+funcionario2.nome = 'Carol'
+funcionario2.sobrenome = 'Silva'
+funcionario2.data_de_nascimento = '15/10/2005'
+
+print(funcionario1.nome)
+print(funcionario2.nome)
