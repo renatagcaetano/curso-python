ce5688966384d7d823ffd1a2d9a7bb655c349a36
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-15 16:44:05 -0300

    Módulo

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,10 @@ def somar():
 
 def multiplicar():
     print('Esta função multiplicará valores.')
+
+
+def encontrar_index(lista, item):
+    for i, valor in enumerate(lista):
+        if valor == item:
+            return i
+    return -1
---
+++ importarmodulo.py
@@ -0,0 +1,6 @@
+from funcoes import encontrar_index
+
+lista1 = ['a', 'b', 'c', 'd', 'e']
+
+index = encontrar_index(lista1, 'b')
+print(index)
