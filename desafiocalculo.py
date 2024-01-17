7536365d492be6a8e6a41dad70f7f23cecff2035
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-17 17:05:15 -0300

    Cálculos

---
+++ desafiocalculo.py
@@ -0,0 +1,10 @@
+from math import pow
+
+number1 = float(input('Informe o primeiro número: '))
+number2 = float(input('Informe o segundo número: '))
+
+print(f'Soma: {number1 + number2}')
+print(f'Subtração: {number1 - number2}')
+print(f'Multiplicação: {number1 * number2}')
+print(f'Divisão: {number1 / number2}')
+print(f'Exponenciação: {pow(number1, number2)}')
