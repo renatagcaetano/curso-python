209bd456ebb16588d4f9ffdd62fc1a2bae92e78f
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-18 15:59:24 -0300

    If elif else

---
+++ desafioifelifelse.py
@@ -0,0 +1,8 @@
+age = int(input('Informe a sua idade: '))
+
+if age < 13:
+    print('Você é uma criança.')
+elif age < 20:
+    print('Você é um adolescente.')
+else:
+    print('Você é um adulto.')
