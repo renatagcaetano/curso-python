eeaeb97e0d1e01d20c9fa46e990da055db71890f
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-18 16:08:52 -0300

    Procurar em lista

---
+++ desafioprocurarlista.py
@@ -0,0 +1,8 @@
+cars = ['BMW X6', 'BMW i5', 'BMW i8']
+car = input('Informe o carro desejado: ')
+
+
+if car in cars:
+    print('Este carro está disponível.')
+else:
+    print('Desculpe, este carro não está disponível.')
