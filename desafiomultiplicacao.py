2147f2e45362906230ee8df3cb0c31af79f4188e
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-19 17:18:18 -0300

    Multiplicação com lambda

---
+++ desafiomultiplicacao.py
@@ -0,0 +1,6 @@
+multiplication = lambda x, y: x * y
+
+number1 = int(input('Informe o valor do primeiro número: '))
+number2 = int(input('Informe o valor do segundo número: '))
+
+print(f'A multiplicação de {number1} por {number2} é {multiplication(number1, number2)}.')
