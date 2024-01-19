6162da6a38e645606ee293cbc0a6407b4729938b
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-19 15:05:02 -0300

    Tuple

---
+++ desafiotuple.py
@@ -0,0 +1,8 @@
+cities = ('Rio de Janeiro', 'São Paulo', 'Salvador')
+
+city = input('Digite o nome de uma cidade: ')
+
+if city in cities:
+    print('A cidade está na lista de cidades.')
+else:
+    print('A cidade não está na lista de cidades.')
