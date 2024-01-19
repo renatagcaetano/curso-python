f7549b88b0da5502cfe2aab3bbe4388f34486833
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-19 15:30:33 -0300

    Intersection

---
+++ desafiointersection.py
@@ -0,0 +1,6 @@
+group1 = {'Ana', 'John', 'Jack', 'Jill', 'Jane'}
+group2 = {'John', 'Vincent', 'Abigail', 'Jack', 'Jane'}
+
+print(group1.intersection(group2))
+print(group1.union(group2))
+print(group1.difference(group2))
