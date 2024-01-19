d683e0fe6b16a71deccb79eb8c7b004a0a4e62ba
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-01-19 16:23:04 -0300

    Função de soma

---
+++ desafiosomafuncao.py
@@ -0,0 +1,8 @@
+def sum(number1, number2):
+    return number1 + number2
+
+
+number1 = float(input('Informe o primeiro número: '))
+number2 = float(input('Informe o segundo número: '))
+
+print(f'A soma dos números {number1} e {number2} é {sum(number1, number2)}')
